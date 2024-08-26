8e165094f025c1b4b273bffe781915e4d4f02d7f
shravan20 <mrshravankumarb@gmail.com>
2024-08-27 02:30:20 +0530

    feat: rename folder path | add standard file structure for restful apis

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,3 @@
-// import express, { type Express, Application } from "express";
-// import { logger, morganMiddleware } from "@helpers/logger";
-
-// const app: Express = express();
-
-// app.set("trust proxy", true);
-
-// app.use(morganMiddleware);
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get("/health-check", (_request, response, _next) => {
-// 	response.send({
-// 		heath: "up",
-// 	});
-// });
-
-// // Error handling middleware
-// app.use((err, _req, res, _next) => {
-// 	logger.error("An error occurred", { error: err.message });
-// 	res.status(500).send("Internal Server Error");
-// });
-
-// export { app, logger };
-
 import { logger, morganMiddleware } from "@helpers/logger";
 import express, { type Express, Application, type Request, type Response, type NextFunction } from "express";
 import { HealthRouter } from "./api/modules/health-check/health-check.router";
