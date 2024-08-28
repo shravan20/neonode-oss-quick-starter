e2819e5eb4aee5376cfc6d166d4be4dbbb2b5c9b
shravan20 <mrshravankumarb@gmail.com>
2024-08-29 02:37:22 +0530

    feat: connect to serverless-psql

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import { logger, morganMiddleware } from "@helpers/logger";
+import dotenv from "dotenv";
 import express, { type Express, Application, type Request, type Response, type NextFunction } from "express";
 import { HealthRouter } from "./api/modules/health-check/health-check.router";
+import { connectToDatabase } from "./db/connect";
 
 export class App {
 	public app: Express;
 
 	constructor() {
+		dotenv.config();
 		this.app = express();
 		this.initializeMiddlewares();
 		this.initializeRoutes();
 		this.initializeErrorHandling();
+
+		this.initializeDatabaseConnection();
 	}
 
 	private initializeMiddlewares(): void {
@@ -32,5 +37,15 @@ export class App {
 				error: err.message,
 			});
 		});
+	}
+
+	private async initializeDatabaseConnection(): Promise<void> {
+		try {
+			await connectToDatabase();
+			logger.info("Connected to the database");
+		} catch (error) {
+			logger.error("Failed to connect to the database", { error: (error as Error).message });
+			process.exit(1);
+		}
 	}
 }
---
+++ src/db/config.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+	type: "postgres",
+	url: process.env.DATABASE_URL,
+	synchronize: true, // TODO: Set to false in production; fix via env config file
+	logging: true,
+	poolSize: 3,
+	entities: [`${__dirname}/entities/*.ts`],
+	migrations: [`${__dirname}/migrations/*.ts`],
+	subscribers: [`${__dirname}/subscribers/*.ts`],
+	ssl: {
+		rejectUnauthorized: false, // Required for Neon.tech SSL connections
+	},
+	extra: {
+		sslmode: "require", // Force SSL mode
+	},
+});
